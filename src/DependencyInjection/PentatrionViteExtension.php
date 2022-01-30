90e36e01b090bf8b59f6180b16e557166a5e9040
Hugues Tavernier <hugues.tavernier@protonmail.com>
2022-01-30 22:37:33 +0100

    fix #12 with vite-plugin-symfony v0.2.3

--- src/DependencyInjection/PentatrionViteExtension.php
+++ src/DependencyInjection/PentatrionViteExtension.php
@@ -5,14 +5,15 @@ namespace Pentatrion\ViteBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class PentatrionViteExtension extends Extension
 {
   public function load(array $configs, ContainerBuilder $container): void
   {
-    $loader = new XmlFileLoader($container, new FileLocator((__DIR__ . '/../Resources/config')));
-    $loader->load('services.xml');
+    $loader = new YamlFileLoader($container, new FileLocator((__DIR__ . '/../Resources/config')));
+    $loader->load('services.yaml');
 
     $config = $this->processConfiguration(
       $this->getConfiguration($configs, $container),

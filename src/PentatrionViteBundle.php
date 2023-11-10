7120e264f409334c57a70fd77a66bdfa44c320f2
Hugues Tavernier <hugues.tavernier@protonmail.com>
2023-11-10 16:53:43 +0100

    change vite-plugin-symfony package.json version

--- src/PentatrionViteBundle.php
+++ src/PentatrionViteBundle.php
@@ -6,5 +6,5 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PentatrionViteBundle extends Bundle
 {
-    public const VERSION = ['6.0.1', 6, 0, 1];
+    public const VERSION = ['6.1.0', 6, 1, 0];
 }

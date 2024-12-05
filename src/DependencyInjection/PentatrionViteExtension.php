deeb3df0329c74bbed97fc7c806d6f0b2d5464cd
Dmitry <dmitry.chubar@fivejars.com>
2024-12-05 22:42:11 +0100

    Change dependency to the non-internal Extension class

--- src/DependencyInjection/PentatrionViteExtension.php
+++ src/DependencyInjection/PentatrionViteExtension.php
@@ -10,7 +10,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\WebLink\EventListener\AddLinkHeaderListener;
 
 /**

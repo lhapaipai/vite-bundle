f2abb9a99e42f8a9169d3e8df715cb72effae810
Hugues Tavernier <hugues.tavernier@protonmail.com>
2024-12-06 11:22:38 +0100

    php-cs-fixer

--- src/DependencyInjection/PentatrionViteExtension.php
+++ src/DependencyInjection/PentatrionViteExtension.php
@@ -8,9 +8,9 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Compiler\ServiceLocatorTagPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\WebLink\EventListener\AddLinkHeaderListener;
 
 /**
